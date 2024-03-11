ba89d1b35565696c57dfb2bb8f8d8e31717647d9
Joel Andrade <joelguarulhos@gmail.com>
2024-03-11 18:18:11 -0300

    Criado projeto inicial no Spring Tool Suite - Spring Initializer embutido'

---
+++ avaliacaoEkan/src/main/java/br/com/joelandrade/avaliacaoekan/AvaliacaoEkanApplication.java
@@ -0,0 +1,13 @@
+package br.com.joelandrade.avaliacaoekan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacaoEkanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacaoEkanApplication.class, args);
+	}
+
+}
